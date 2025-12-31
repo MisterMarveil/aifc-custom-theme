bab403d2a850ffed528736039285d8f7cb56616b
Mister Marveil <ewoniewonimerveil@gmail.com>
2025-12-31 05:39:09 +0100

    adding modes

--- views/content-single-project.php
+++ views/content-single-project.php
@@ -30,7 +30,7 @@ $rt_project_rating = 5- intval( $ratting );
 				<?php 
 				// Remplacer tout le contenu existant par :
 				echo do_shortcode('[widget_info_formation compact="yes"]');
-				echo do_shortcode('[formulaire_brochure]');
+				//echo do_shortcode('[formulaire_brochure]');
 				
 				
 				// Vous pouvez ajouter d'autres widgets si nécessaire
