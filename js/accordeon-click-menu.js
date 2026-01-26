e4ebe482820bd18098742f356b53c3a796734da9
Mister Marveil <ewoniewonimerveil@gmail.com>
2026-01-26 13:33:22 +0100

    simulate click on dest element for services side bar menu

--- js/accordeon-click-menu.js
+++ js/accordeon-click-menu.js
@@ -15,7 +15,7 @@
                     if( !$href.startsWith("#") ) {
                         return; // Ne pas interférer avec les liens externes
                     }
-                    e.preventDefault();
+                    //e.preventDefault();
                     var destElement = document.getElementById($href.substring(1));
                     destElement.click(); // Simuler le clic sur l'élément cible                                        
                 });
